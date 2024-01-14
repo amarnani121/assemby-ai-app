07fa662063c9ebcfa5a3530b55b6ed6df6430139
amarnani121 <72928853+amarnani121@users.noreply.github.com>
2024-01-14 09:50:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ from pytube import YouTube
 from utilities import get_yt, transcribe_yt
 from PIL import Image
 
-img = Image.open("https://amarnani.000webhostapp.com/wp-content/uploads/2024/01/668579e4-fb37-40bd-b367-03a829bf0193-1-643x1024.jpg")
-st.image(img)
+
+st.image(https://amarnani.000webhostapp.com/wp-content/uploads/2024/01/668579e4-fb37-40bd-b367-03a829bf0193-1-643x1024.jpg)
 st.markdown('# 📝 ** Content extraction App** ')
 st.markdown('-created by amaresh')
 st.warning('waiting for URL input in the left sidebar.')
